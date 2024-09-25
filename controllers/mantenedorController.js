31f9a3a8fbb70ed15047abbc23dfae1f9b93299d
italorojass <italo.rojas@nextcode.cl>
2024-09-25 13:33:31 -0300

    hola mundo

--- controllers/mantenedorController.js
+++ controllers/mantenedorController.js
@@ -1,260 +1,132 @@
 const { executeQuery } = require('../conexions/database');
 const { connectToDatabase } = require('../conexions/database');
 
-const getDisciplinas = async (req, res) => {
-  const query = 'SELECT * FROM disciplines WHERE State = 1;';
-  try {
-    const resultQuery = await executeQuery(query, []);
-
-    if (resultQuery.length === 0) {
-      return res.status(400).json({ message: 'Sin disciplinas' });
-    }
-
-    res.status(200).json(resultQuery);
-  } catch (error) {
-    res.status(500).json({ message: 'Error al obtener las disciplinas', error });
-  }
-}
-
-const getHorarios = async (req, res) => {
-  const { disciplineID } = req.params;
-  // Verificar si hay múltiples IDs o solo uno
-  const idsArray = disciplineID.includes(',')
-    ? disciplineID.split(',').map(id => parseInt(id.trim(), 10))
-    : [parseInt(disciplineID.trim(), 10)];
-  // Generar la parte de la consulta para los IDs
-  const placeholders = idsArray.map(() => '?').join(',');
-
-  const query = `SELECT 
-    t.TimeID,
-    t.TimeValue,
-    GROUP_CONCAT(CONCAT(d.DisciplineName, ' (', dd.DayName, ')') ORDER BY dd.DayName SEPARATOR ', ') AS Disciplines
-FROM 
-    schedules AS s
-INNER JOIN 
-    disciplines AS d ON d.DisciplineID = s.DisciplineID
-INNER JOIN 
-    Days AS dd ON dd.DayID = s.DayID
-INNER JOIN 
-    Times AS t ON s.TimeID = t.TimeID
-WHERE 
-    d.DisciplineID IN (${placeholders})
-GROUP BY 
-    t.TimeID, t.TimeValue
-ORDER BY 
-    t.TimeID;`;
-  try {
-    const resultQuery = await executeQuery(query, idsArray);
-
-    res.status(200).json(resultQuery);
-  } catch (error) {
-    res.status(500).json({ message: 'Error al obtener horarios', error });
-  }
 
 
-}
+
 
 // Función para insertar un nuevo alumno y asociarlo con planes y disciplinas
 const postAlumnoNuevo = async (req, res) => {
-  const { name, email, enrollmentDate, plans, disciplines } = req.body;
+   const { nombre, apellido, email, telefono, telefono_emergencia, direccion, comuna, disciplina, plan, observaciones, fecha_registro } = req.body;
+   try {
+    // Convertir el array de disciplinas a JSON
+    const disciplinasJson = JSON.stringify(disciplina);
 
-  if (!name || !email || !enrollmentDate || !Array.isArray(plans) || !Array.isArray(disciplines)) {
-    return res.status(400).json({ message: 'Todos los campos son requeridos y plans y disciplines deben ser arrays' });
-  }
-
-  const connection = await connectToDatabase();
-
-  try {
-    await connection.beginTransaction();
-
-    // Insertar nuevo alumno
-    const insertStudentQuery = `
-        INSERT INTO Students (Name, Email, EnrollmentDate)
-        VALUES (?, ?, ?);
-      `;
-    const [studentResult] = await connection.execute(insertStudentQuery, [name, email, enrollmentDate]);
-    const studentID = studentResult.insertId;
-
-    // Asociar alumno con planes
-    const insertStudentPlanQuery = `
-        INSERT INTO StudentPlans (StudentID, PlanID)
-        VALUES (?, ?);
-      `;
-
-    for (const planID of plans) {
-      await connection.execute(insertStudentPlanQuery, [studentID, planID]);
-    }
-
-    // Asociar alumno con disciplinas
-    const insertStudentDisciplineQuery = `
-        INSERT INTO StudentDisciplines (StudentID, DisciplineID)
-        VALUES (?, ?);
-      `;
-
-    for (const disciplineID of disciplines) {
-      await connection.execute(insertStudentDisciplineQuery, [studentID, disciplineID]);
-    }
-
-    // Crear recordatorio de pago
-    const currentDate = new Date();
-    const paymentDueDate = new Date();
-    paymentDueDate.setDate(currentDate.getDate() + 30);
-
-    const insertPaymentReminderQuery = `
-      INSERT INTO PaymentReminders (StudentID, ReminderDate, PaymentDueDate)
-      VALUES (?, ?, ?);
-    `;
-
-    await connection.execute(insertPaymentReminderQuery, [studentID, currentDate, paymentDueDate]);
+   await executeQuery(
+        `CALL crear_alumno(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, @nuevo_alumno_id)`,
+        [nombre, apellido, email, telefono, telefono_emergencia, direccion, comuna, disciplinasJson, plan, observaciones, fecha_registro]
+    );
 
 
-    // Commit the transaction
-    await connection.commit();
-    res.status(201).json({ message: 'Alumno registrado exitosamente' });
-  } catch (error) {
-    // Rollback the transaction in case of error
-    await connection.rollback();
-    res.status(500).json({ message: 'Error al registrar el alumno', error });
-  } finally {
-    await connection.end();
-  }
+      /*   // Obtener el nuevo ID del alumno registrado
+        const [rows] = await executeQuery(`SELECT @nuevo_alumno_id AS alumno_id`);
+        const nuevoAlumnoId = rows[0].alumno_id;
+
+        // Ahora registrar un pago para el nuevo alumno
+        const monto_pago_inicial = 100; // O el monto que necesites para el pago inicial
+        await connection.execute(
+            `CALL registrar_pago(?, ?)`,
+            [nuevoAlumnoId, monto_pago_inicial],
+            
+        ); */
+
+    res.status(200).json({
+        message: 'Alumno creado y asociado a las disciplinas con éxito'
+    });
+} catch (error) {
+    console.error('Error al crear alumno:', error);
+    res.status(500).json({
+        message: 'Error al crear alumno',
+        error: error.message
+    });
+} 
+
 };
 
-const getAlumnos = async (req, res) => {
-  const query = `
-      SELECT 
-    d.DisciplineName,
-    s.StudentID,
-    s.Name AS StudentName,
-    s.EnrollmentDate,
-    pr.PaymentDueDate,
-    i.Name AS InstructorName,
-    p.PlanName,
-    p.Price
-FROM 
-    Students s
-INNER JOIN 
-    StudentDisciplines sd ON s.StudentID = sd.StudentID
-INNER JOIN 
-    Disciplines d ON sd.DisciplineID = d.DisciplineID
-INNER JOIN 
-    StudentPlans sp ON s.StudentID = sp.StudentID
-INNER JOIN 
-    Plans p ON sp.PlanID = p.PlanID
-INNER JOIN 
-    DisciplineInstructors di ON d.DisciplineID = di.DisciplineID
-INNER JOIN 
-    Instructors i ON di.InstructorID = i.InstructorID
-INNER JOIN 
-    PaymentReminders pr ON s.StudentID = pr.StudentID
-    WHERE p.planName !='Matricula'
-GROUP BY 
-    d.DisciplineName, s.StudentID, s.Name, s.EnrollmentDate, pr.PaymentDueDate, i.Name, p.PlanName, p.Price
-ORDER BY 
-    d.DisciplineName, s.Name;
-
-    `;
+const registrarPago = async(req,res)=>{
+  const { alumno_id, monto_pago } = req.body;
 
   try {
-    const results = await executeQuery(query);
+    const [result] = await connection.execute(
+        `CALL registrar_pago(?, ?)`,
+        [alumno_id, monto_pago]
+    );
+
+    res.status(200).json({
+        message: 'Pago registrado con éxito y fecha de renovación actualizada',
+        data: result
+    });
+} catch (error) {
+    console.error('Error al registrar pago:', error);
+    res.status(500).json({
+        message: 'Error al registrar pago',
+        error: error.message
+    });
+} 
+
+}
+
+const getAlumnos = async (req, res) => {
+  const query = `CALL obtener_alumnos_inscritos()`;
+
+  try {
+    const [results] = await executeQuery(query);
     res.status(200).json(results);
   } catch (error) {
     res.status(500).json({ message: 'Error al obtener los alumnos', error });
   }
 };
 
-// Mantenedor de Disciplinas
-const createDisciplina = async (req, res) => {
-  const { DisciplineName } = req.body;
-  const query = `INSERT INTO Disciplines (DisciplineName) VALUES (?);`;
-  try {
-    const result = await executeQuery(queryDisciplina, [DisciplineName]);
-    const disciplineId = result.insertId;
-    const queryAssociation = `INSERT INTO DisciplineInstructors (DisciplineID, InstructorID) VALUES (?, ?);`;
-    await executeQuery(queryAssociation, [disciplineId, InstructorID]);
 
-    res.status(201).json({ message: 'Disciplina creada exitosamente' });
-  } catch (error) {
-    res.status(500).json({ message: 'Error al crear disciplina', error });
-  }
-};
 
-const getDisciplinasMantenedor = async (req, res) => {
-  const query = `SELECT 
-    d.DisciplineID,
-    d.DisciplineName,
-    JSON_ARRAYAGG(
-        JSON_OBJECT(
-            'InstructorID', IFNULL(i.InstructorID, 'N/A'),
-            'InstructorName', IFNULL(i.Name, 'N/A')
-        )
-    ) AS Instructors,
-    JSON_ARRAYAGG(
-        JSON_OBJECT(
-            'ScheduleID', IFNULL(sc.ScheduleID, 'N/A'),
-            'Day', IFNULL(days.DayName, 'N/A'),
-            'Time', IFNULL(t.TimeValue, 'N/A')
-        )
-    ) AS Schedules
-FROM 
-    Disciplines d
-LEFT JOIN 
-    DisciplineInstructors di ON d.DisciplineID = di.DisciplineID
-LEFT JOIN 
-    Instructors i ON di.InstructorID = i.InstructorID
-LEFT JOIN 
-    Schedules sc ON d.DisciplineID = sc.DisciplineID
-LEFT JOIN 
-    Times t ON sc.TimeID = t.TimeID
-LEFT JOIN 
-    Days days ON sc.DayID = days.DayID
-GROUP BY 
-    d.DisciplineID, d.DisciplineName
-ORDER BY 
-    d.DisciplineName;`;
+const getComunas = async (req, res) => {
+ 
+  const query = `call obtener_comunas_por_region()`;
+  
   try {
-    const results = await executeQuery(query);
+    const [results] = await executeQuery(query);
     res.status(200).json(results);
   } catch (error) {
-    res.status(500).json({ message: 'Error al obtener disciplinas', error });
+    res.status(500).json({ message: 'Error al obtener comunas', error : error.message });
   }
 };
 
-const updateDisciplina = async (req, res) => {
-  const { id } = req.params;
-  const { DisciplineName } = req.body;
-  const query = `UPDATE Disciplines SET DisciplineName = ? WHERE DisciplineID = ?;`;
-  try {
-    await executeQuery(query, [DisciplineName, id]);
-    res.status(200).json({ message: 'Disciplina actualizada exitosamente' });
-  } catch (error) {
-    res.status(500).json({ message: 'Error al actualizar disciplina', error });
-  }
-};
-
-const deleteDisciplina = async (req, res) => {
-  const { id } = req.params;
-  const query = `DELETE FROM Disciplines WHERE DisciplineID = ?;`;
-  try {
-    await executeQuery(query, [id]);
-    res.status(200).json({ message: 'Disciplina eliminada exitosamente' });
-  } catch (error) {
-    res.status(500).json({ message: 'Error al eliminar disciplina', error });
-  }
-};
 
 // Mantenedor de Planes
 const createPlan = async (req, res) => {
-  const { PlanName, Price } = req.body;
-  const query = `INSERT INTO Plans (PlanName, Price) VALUES (?, ?);`;
+  const { plan, tipo,precio } = req.body;
+  const query = `call agregar_plan(?,?,?)`;
   try {
-    await executeQuery(query, [PlanName, Price]);
+    await executeQuery(query, [plan, tipo,precio]);
     res.status(201).json({ message: 'Plan creado exitosamente' });
   } catch (error) {
     res.status(500).json({ message: 'Error al crear plan', error });
   }
 };
 
+const deletePlan = async (req,res) =>{
+  const  id  = req.params.id;
+  const query = `call eliminar_plan(?)`;
+  try {
+    await executeQuery(query, [id]);
+    res.status(201).json({ message: 'Plan eliminado exitosamente' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error al eliminar plan', error });
+  }
+}
+
+const updatePlan = async (req,res) =>{
+  const { precio } = req.body;
+  const planId = req.params.id;
+ 
+  const query = `call actualizar_plan(?,?)`;
+  try {
+    await executeQuery(query, [planId,precio]);
+    res.status(201).json({ message: 'Plan actualizado exitosamente' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error al actualizar plan', error });
+  }
+}
 // Similarmente se implementan getPlanes, updatePlan, deletePlan
 
 // Mantenedor de Instructores
@@ -273,11 +145,26 @@ const createInstructor = async (req, res) => {
 
 // Mantenedor de Horarios
 const createHorario = async (req, res) => {
-  const { DisciplineID, DayID, TimeID, InstructorID } = req.body;
-  const query = `INSERT INTO Schedules (DisciplineID, DayID, TimeID, InstructorID) VALUES (?, ?, ?, ?);`;
+  const { dias, horas } = req.body;
+    // Convertir la lista de días a JSON
+    
+    const diasJson = JSON.stringify(dias);
+
+    console.log(diasJson);
+
   try {
-    await executeQuery(query, [DisciplineID, DayID, TimeID, InstructorID]);
-    res.status(201).json({ message: 'Horario creado exitosamente' });
+    for (const dia_id of dias) {
+      await executeQuery(
+        `CALL crear_horario(?, ?)`,
+        [dia_id, horas]
+    );
+    }
+    
+
+  res.status(200).json({
+    message: 'Horario creado con éxito',
+    //data: results
+});
   } catch (error) {
     res.status(500).json({ message: 'Error al crear horario', error });
   }
@@ -285,30 +172,8 @@ const createHorario = async (req, res) => {
 
 // Similarmente se implementan getHorarios, updateHorario, deleteHorario
 
-// Mantenedor de Precios de Disciplinas
-const setDisciplinaPrecio = async (req, res) => {
-  const { id } = req.params; // DisciplineID
-  const { Price } = req.body;
-  const query = `UPDATE Disciplines SET Price = ? WHERE DisciplineID = ?;`;
-  try {
-    await executeQuery(query, [Price, id]);
-    res.status(200).json({ message: 'Precio actualizado exitosamente' });
-  } catch (error) {
-    res.status(500).json({ message: 'Error al actualizar precio de la disciplina', error });
-  }
-};
 
-// Asociación de Disciplinas a Instructores
-const associateDisciplinaInstructor = async (req, res) => {
-  const { disciplinaId, instructorId } = req.params;
-  const query = `INSERT INTO DisciplineInstructors (DisciplineID, InstructorID) VALUES (?, ?);`;
-  try {
-    await executeQuery(query, [disciplinaId, instructorId]);
-    res.status(201).json({ message: 'Disciplina asociada a instructor exitosamente' });
-  } catch (error) {
-    res.status(500).json({ message: 'Error al asociar disciplina a instructor', error });
-  }
-};
+
 
 const getDias = async (req, res) => {
   const query = `SELECT * FROM days;`;
@@ -320,10 +185,10 @@ const getDias = async (req, res) => {
   }
 }
 
-const getTimes = async (req, res) => {
-  const query = `SELECT * FROM times;`;
+const getHorarioTabla = async (req, res) => {
+ 
   try {
-    const result = await executeQuery(query);
+    const [result] = await executeQuery(`CALL obtener_horarios_mantenedor()`);
     res.status(201).json(result);
   } catch (error) {
     res.status(500).json({ message: 'Error al obtener horarios', error });
@@ -333,29 +198,40 @@ const getTimes = async (req, res) => {
 
 
 const getPlanes = async (req, res) => {
-  const query = `SELECT * FROM plans;`;
+  const query = `CALL obtener_planes();`;
   try {
-    const result = await executeQuery(query);
+    const [result] = await executeQuery(query);
     res.status(201).json(result);
   } catch (error) {
     res.status(500).json({ message: 'Error al obtener horarios', error });
   }
 }
+
+const deleteAlumno = async (req,res) =>{
+  const  id  = req.params.id;
+  const query = `call eliminar_alumno(?)`;
+  try {
+    await executeQuery(query, [id]);
+    res.status(201).json({ message: 'Plan eliminado exitosamente' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error al eliminar plan', error });
+  }
+}
+
+
 module.exports = {
-  getDisciplinas,
-  getDisciplinasMantenedor,
-  getHorarios,
+  deleteAlumno,
+  getComunas,
+  getHorarioTabla,
   getDias,
-  getTimes,
+  registrarPago,
+  //planes
+  createPlan,
   getPlanes,
+  deletePlan,
+  updatePlan,
+  //planes
   postAlumnoNuevo,
   getAlumnos,
-  createDisciplina,
-  updateDisciplina,
-  deleteDisciplina,
-  createPlan,
-  createInstructor,
   createHorario,
-  setDisciplinaPrecio,
-  associateDisciplinaInstructor
 };
--- routes/index.js
+++ routes/index.js
@@ -16,20 +16,18 @@ router.post('/login', authController.login);
 router.post('/register', authController.register);
 
 // Rutas KEYS Mantendor
-router.get('/keys/disciplinas',authenticate,mantenedorController.getDisciplinas);
-router.get('/keys/horarios/:disciplineID',authenticate,mantenedorController.getHorarios);
-
+router.get('/keys/disciplinas',authenticate,disciplinaController.getDisciplinas);
+router.get('/keys/comunas/',authenticate,mantenedorController.getComunas);
 //Globales
 router.post('/alumno', authenticate, mantenedorController.postAlumnoNuevo);
 router.get('/alumno', authenticate, mantenedorController.getAlumnos);
+router.delete('/alumno/:id', authenticate, mantenedorController.deleteAlumno);
 
 // Rutas para Disciplinas (solo admin puede crear, actualizar, eliminar)
-router.post('/disciplinas', authenticate, isAdmin, mantenedorController.createDisciplina);
-router.get('/disciplinas', authenticate, mantenedorController.getDisciplinas);
-router.get('/disciplinasMantenedor', authenticate, mantenedorController.getDisciplinasMantenedor);
-
-router.put('/disciplinas/:id', authenticate, isAdmin, mantenedorController.updateDisciplina);
-router.delete('/disciplinas/:id', authenticate, isAdmin, mantenedorController.deleteDisciplina);
+router.post('/disciplinas', authenticate, isAdmin, disciplinaController.agregarDisciplina);
+router.get('/disciplinas', authenticate, disciplinaController.getDisciplinas);
+router.put('/disciplinas/:id', authenticate, isAdmin, disciplinaController.updateDisciplina);
+router.delete('/disciplinas/:id', authenticate, isAdmin, disciplinaController.deleteDisciplina);
 
 
 router.get('/dias', authenticate, mantenedorController.getDias);
@@ -37,31 +35,29 @@ router.get('/times', authenticate, mantenedorController.getTimes);
 
 router.get('/estadisticas', authenticate, dashboardController.getEstadisticasPorDisciplina);
 
+
 // Rutas para Instructores (solo admin puede crear, actualizar, eliminar)
 //router.post('/instructores', authenticate, isAdmin, mantenedorController.createInstructor);
 router.get('/instructores', authenticate, instructorController.getInstructores);
 //router.put('/instructores/:id', authenticate, isAdmin, mantenedorController.updateInstructor);
 //router.delete('/instructores/:id', authenticate, isAdmin, mantenedorController.deleteInstructor);
 
-/* 
 // Rutas para Planes (solo admin puede crear, actualizar, eliminar)
-router.post('/planes', authenticate, isAdmin, mantenedorController.createPlan);
+
 router.get('/planes', authenticate, mantenedorController.getPlanes);
+router.post('/planes', authenticate, isAdmin, mantenedorController.createPlan);
+router.delete('/planes/:id', authenticate, mantenedorController.deletePlan);
 router.put('/planes/:id', authenticate, isAdmin, mantenedorController.updatePlan);
-router.delete('/planes/:id', authenticate, isAdmin, mantenedorController.deletePlan);
 
 
 
 // Rutas para Horarios (solo admin puede crear, actualizar, eliminar)
 router.post('/horarios', authenticate, isAdmin, mantenedorController.createHorario);
-router.get('/horarios', authenticate, mantenedorController.getHorarios);
-router.put('/horarios/:id', authenticate, isAdmin, mantenedorController.updateHorario);
+router.get('/horarios', authenticate, mantenedorController.getHorarioTabla);
+/* router.put('/horarios/:id', authenticate, isAdmin, mantenedorController.updateHorario);
 router.delete('/horarios/:id', authenticate, isAdmin, mantenedorController.deleteHorario);
-
-// Rutas para Precios de Disciplinas (solo admin puede actualizar)
-router.post('/disciplinas/:id/precio', authenticate, isAdmin, mantenedorController.setDisciplinaPrecio);
-router.put('/disciplinas/:id/precio', authenticate, isAdmin, mantenedorController.updateDisciplinaPrecio);
-
+ */
+/* 
 // Ruta para asociar Disciplinas a Instructores (solo admin puede asociar)
 router.post('/disciplinas/:disciplinaId/instructores/:instructorId', authenticate, isAdmin, mantenedorController.associateDisciplinaInstructor); */
 
